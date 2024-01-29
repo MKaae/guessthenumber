22df4ecf4a5fbfec816a8e8b27aa1e035200d5b1
zivbabs <117026503+zivbabs@users.noreply.github.com>
2024-01-29 13:50:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", start);
+
+function start() {
+    console.log("start");
+}
+// 1. Start med at lave HTML’en hvordan den skal se ud efter et par forkerte gæt, og lav også knapperne.
+// 2. Gå så i gang med JavaScripten, lav main-funktionen, og tjek at den udskriver noget i konsollen når siden bliver loadet.
+// 3. Tilføj en funktion der laver eventlistenere på knapperne, og også de tre funktioner der bliver kaldt når man trykker på hver knap.
+// 4. Lav en funktion til at lave et nyt gæt - og en anden funktion til at vise dette gæt på skærmen.
+// 5. Tilret i funktionerne for “for højt”, “for lavt” og “korrekt”, så de viser en besked ved det seneste gæt.
+// 6. Lav funktionerne så programmet laver et nyt gæt ved “for højt” og “for lavt”, men stopper ved “korrekt”
