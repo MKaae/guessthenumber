23dc084d8f6dc8afae9d1d0607d8a4a792ee766e
zivbabs <117026503+zivbabs@users.noreply.github.com>
2024-01-29 15:05:16 +0100

    first prod

--- script.js
+++ script.js
@@ -1,9 +1,44 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", start);
+let randomNumber = "";
 
 function start() {
-    console.log("start");
+    generateRandomNumber();
+    document.querySelector('#guess-form').addEventListener('submit', recieveGuess);
+}
+function generateRandomNumber(){
+    randomNumber = Math.floor(Math.random() * 99) + 1;
+}
+function recieveGuess(event){
+    event.preventDefault();
+    const form = event.target;
+    const guess = form.guess.valueAsNumber;
+    form.guess.value = "";
+    compareGuess(guess);
+}
+function compareGuess(guess){
+    if (guess === randomNumber){
+        guessIsCorrect(guess);
+    } else if (guess > randomNumber){
+        guessIsTooHigh(guess);
+    } else if (guess < randomNumber){
+        guessIsTooLow(guess);
+    }
+}
+function guessIsCorrect(guess){
+    const list = document.querySelector('#guess-list');
+    list.insertAdjacentHTML('beforeend', `<li>You guessed ${guess} that was correct!</li>`)
+    document.querySelector('#guess').disabled = true;
+    document.querySelector('#guess-btn').disabled = true;
+}
+function guessIsTooHigh(guess){
+    const list = document.querySelector('#guess-list');
+    list.insertAdjacentHTML('beforeend', `<li>You guessed ${guess} that was too high - try again</li>`)
+}
+function guessIsTooLow(guess){
+    const list = document.querySelector('#guess-list');
+    list.insertAdjacentHTML('beforeend', `<li>You guessed ${guess} that was too low - try again</li>`)
 }
 // 1. Start med at lave HTML’en hvordan den skal se ud efter et par forkerte gæt, og lav også knapperne.
 // 2. Gå så i gang med JavaScripten, lav main-funktionen, og tjek at den udskriver noget i konsollen når siden bliver loadet.
